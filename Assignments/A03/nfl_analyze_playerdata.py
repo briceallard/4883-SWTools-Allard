c31452bd5736d5e37147aa1fb54d934477d0cbeb
Brice Allard <briceallard@gmail.com>
2019-02-20 03:27:29 -0600

    finished more assignment requirements

--- Assignments/A03/nfl_analyze_playerdata.py
+++ Assignments/A03/nfl_analyze_playerdata.py
@@ -68,7 +68,7 @@ def mostTeamsCareer(files):
 
 
 # Find the player(s) that had the most yards rushed for a loss.
-def mostRushMinus(files):
+def mostRushYardMinus(files):
     players = {}
 
     for file in files:
@@ -96,9 +96,117 @@ def mostRushMinus(files):
     return top_five
 
 # Find the player(s) that had the most rushes for a loss.
+def mostRushMinus(files):
+    players = {}
+
+    for file in files:
+        data = openFileJson(file)
+
+        for stat in data['stats']:
+            if data['playerName'] not in players.keys():
+                if stat['statId'] == 10:
+                    players[data['playerName']] = {}
+                    players[data['playerName']]['totYds'] = 0
+                    players[data['playerName']]['count'] = 0
+
+            elif stat['statId'] != None and stat['yards'] != None:
+                if stat['statId'] == 10 and stat['yards'] < 0:
+                    players[data['playerName']]['totYds'] += stat['yards']
+                    players[data['playerName']]['count'] += 1
+
+    sorted_dict = OrderedDict()
+    sorted_keys = sorted(players, key=lambda x: players[x]['count'])
+
+    for key in sorted_keys:
+        sorted_dict[key] = players[key]
+
+    top_five = list(islice(sorted_dict.items(), 5))
+    return top_five
+
 # Find the player(s) with the most number of passes for a loss.
+def mostPassMinus(files):
+    players = {}
+
+    for file in files:
+        data = openFileJson(file)
+
+        for stat in data['stats']:
+            if data['playerName'] not in players.keys():
+                if stat['statId'] == 21:
+                    players[data['playerName']] = {}
+                    players[data['playerName']]['totYds'] = 0
+                    players[data['playerName']]['count'] = 0
+
+            elif stat['statId'] != None and stat['yards'] != None:
+                if stat['statId'] == 21 and stat['yards'] < 0:
+                    players[data['playerName']]['totYds'] += stat['yards']
+                    players[data['playerName']]['count'] += 1
+
+    sorted_dict = OrderedDict()
+    sorted_keys = sorted(players, key=lambda x: players[x]['totYds'])
+
+    for key in sorted_keys:
+        sorted_dict[key] = players[key]
+
+    top_five = list(islice(sorted_dict.items(), 5))
+    return top_five
+    
 # Find the team with the most penalties.
+def mostTeamPenalties(files):
+    teams = {}
+
+    for file in files:
+        data = openFileJson(file)
+
+        for stat in data['stats']:
+            if stat['clubcode'] not in teams.keys():
+                if stat['statId'] == 93:
+                    teams[stat['clubcode']] = {}
+                    teams[stat['clubcode']]['totYds'] = 0
+                    teams[stat['clubcode']]['count'] = 0
+
+            elif stat['statId'] != None and stat['yards'] != None:
+                if stat['statId'] == 93:
+                    teams[stat['clubcode']]['totYds'] += stat['yards']
+                    teams[stat['clubcode']]['count'] += 1
+
+    sorted_dict = OrderedDict()
+    sorted_keys = sorted(teams, key=lambda x: teams[x]['count'])
+
+    for key in sorted_keys:
+        sorted_dict[key] = teams[key]
+
+    top_five = list(islice(sorted_dict.items(), len(sorted_dict) - 5, len(sorted_dict)))
+    return top_five
+
 # Find the team with the most yards in penalties.
+def mostYardsPenalties(files):
+    teams = {}
+
+    for file in files:
+        data = openFileJson(file)
+
+        for stat in data['stats']:
+            if stat['clubcode'] not in teams.keys():
+                if stat['statId'] == 93:
+                    teams[stat['clubcode']] = {}
+                    teams[stat['clubcode']]['totYds'] = 0
+                    teams[stat['clubcode']]['count'] = 0
+
+            elif stat['statId'] != None and stat['yards'] != None:
+                if stat['statId'] == 93:
+                    teams[stat['clubcode']]['totYds'] += stat['yards']
+                    teams[stat['clubcode']]['count'] += 1
+
+    sorted_dict = OrderedDict()
+    sorted_keys = sorted(teams, key=lambda x: teams[x]['totYds'])
+
+    for key in sorted_keys:
+        sorted_dict[key] = teams[key]
+
+    top_five = list(islice(sorted_dict.items(), len(sorted_dict) - 5, len(sorted_dict)))
+    return top_five
+
 # Find the correlation between most penalized teams and games won / lost.
 # Average number of plays in a game.
 # Longest field goal.
@@ -113,4 +221,16 @@ print("\nFind the player(s) that played for the most teams:")
 pprint(mostTeamsCareer(files))
 
 print("\nFind the player(s) that had the most yards rushed for a loss:")
-pprint(mostRushMinus(files))
+pprint(mostRushYardMinus(files))
+
+print("\nFind the player(s) that had the most rushes for a loss:")
+pprint(mostRushYardMinus(files))
+
+print("\nFind the player(s) with the most number of passes for a loss:")
+pprint(mostPassMinus(files))
+
+print("\nFind the team with the most penalties:")
+pprint(mostTeamPenalties(files))
+
+print("\nFind the team with the most yards in penalties:")
+pprint(mostYardsPenalties(files))
