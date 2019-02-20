db658538be179e2498ccab9a2b5086b3f5cf65b3
Brice Allard <briceallard@gmail.com>
2019-02-19 21:24:04 -0600

    Added totals to dictionary for easier lookup

--- Assignments/A03/nfl_scrape_playerdata.py
+++ Assignments/A03/nfl_scrape_playerdata.py
@@ -8,13 +8,14 @@ read_path = os.path.dirname(os.path.abspath(__file__)) + '/data/game_data/'
 write_path = os.path.dirname(os.path.abspath(__file__)) + '/data/playerdata/'
 
 players = {}
-totStat = {}
 
 file_read_count = 1
-file_total_count = len([f for f in os.listdir(read_path) if os.path.isfile(os.path.join(read_path, f))])
+file_total_count = len([f for f in os.listdir(
+    read_path) if os.path.isfile(os.path.join(read_path, f))])
 
 for filename in os.listdir(read_path):
-    print("Gathering data - {0} of {1} - {2}%".format(file_read_count, file_total_count, math.trunc(file_read_count / file_total_count * 100)))
+    print("Gathering data - {0} of {1} - {2}%".format(file_read_count,
+                                                      file_total_count, math.trunc(file_read_count / file_total_count * 100)))
 
     with open(read_path + filename) as f:
         data = json.load(f)
@@ -25,17 +26,18 @@ for filename in os.listdir(read_path):
                 for drive_id, drive_data in game_data['drives'].items():
                     if drive_id != "crntdrv":
 
+                        
                         for play_id, play_data in drive_data['plays'].items():
 
                             for player_id, player_data in play_data['players'].items():
                                 if player_id != "0":
-                                    
+                                    totals = {'statId': 0, 'count': 0, 'totYds': 0}
+
                                     if player_id not in players.keys():
                                         players[player_id] = {}
                                         players[player_id]['teams'] = []
                                         players[player_id]['stats'] = []
                                         players[player_id]['totals'] = {}
-                                        players[player_id]['totals']['statId'] = []
 
                                     for stat in player_data:
                                         stat['gameId'] = game_id
@@ -44,31 +46,34 @@ for filename in os.listdir(read_path):
 
                                         if stat['clubcode'] not in players[player_id]['teams']:
                                             players[player_id]['teams'].append(stat['clubcode'])
+                                        
+                                        # Update totals for each player with statId, totYds, and count
+                                        if stat['statId'] not in players[player_id]['totals']:
+                                            players[player_id]['totals'][stat['statId']] = {
+                                                'statId':stat['statId'],
+                                                'count': 1,
+                                                'totYds': stat['yards']
+                                            }
+                                        elif stat['yards'] != None and players[player_id]['totals'][stat['statId']]['totYds'] != None:
+                                            players[player_id]['totals'][stat['statId']]['statId'] = stat['statId']
+                                            players[player_id]['totals'][stat['statId']]['count'] += 1
+                                            players[player_id]['totals'][stat['statId']]['totYds'] += stat['yards']
 
-                                        if stat['statId'] not in players[player_id]['totals']['statId']:
-                                            players[player_id]['totals']['statId'] = stat['statId']
-                                            totStat['statId'] = stat['statId']
-                                            totStat['totYds'] = 0
-                                            totStat['count'] = 0
-                                            players[player_id]['totals']['statId'] = totStat
-                                        else:
-                                            totStat['totYds'] += stat['yards']
-                                            totStat['count'] += 1
-                                            players[player_id]['totals']['statId'] = totStat
-    
-    file_read_count+=1
+    file_read_count += 1
 
-    pprint(players)
-    sys.exit()
+    # pprint(players)
+    # sys.exit()
 
-# for key in players:
-#     filename = '%s' % (key) + '.json'
+print("Writing files to {0}".format(write_path))
 
-#     if not os.path.exists(write_path):
-#         os.makedirs(write_path)
+for key in players:
+    filename = '%s' % (key) + '.json'
 
-#     w = open(write_path + filename, 'w+')
-#     w.write(json.dumps(players[key]))
-#     w.close()
+    if not os.path.exists(write_path):
+        os.makedirs(write_path)
+
+    w = open(write_path + filename, 'w+')
+    w.write(json.dumps(players[key]))
+    w.close()
 
 f.close()
